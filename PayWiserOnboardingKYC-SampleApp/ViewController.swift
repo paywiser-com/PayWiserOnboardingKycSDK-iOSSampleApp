b5327e876f29772fb245178bb77dd36ec43037ab
tjasa <tjasaa@gmail.com>
2020-07-29 11:43:49 +0200

    Initial Commit

---
+++ PayWiserOnboardingKYC-SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayWiserOnboardingKYC-SampleApp
+//
+//  Created by tjasa on 29/07/2020.
+//  Copyright © 2020 Intech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
